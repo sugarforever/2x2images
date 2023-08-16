106a023df7bce601d46d91a1de2e8b42001de782
Weili Yang <william.1022@gmail.com>
2023-08-16 19:07:37 +0100

    token input

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.title("2x2 Images")
 def get_progress_text(percentage):
     return f"Image generation in progress ({percentage}%). Please wait."
 
-def imagine(prompt_input):
+def imagine(prompt_input, thenextleg_token):
     url = "https://api.thenextleg.io/v2/imagine"
     payload = json.dumps({
         "msg": prompt_input,
@@ -20,7 +20,7 @@ def imagine(prompt_input):
         "webhookOverride": ""
     })
     headers = {
-        'Authorization': f'Bearer {os.environ["THE_NEXT_LEG_TOKEN"]}',
+        'Authorization': f'Bearer {thenextleg_token}',
         'Content-Type': 'application/json'
     }
 
@@ -28,15 +28,16 @@ def imagine(prompt_input):
     response = requests.request("POST", url, headers=headers, data=payload)
     if (response.status_code == 200):
         json_response = json.loads(response.text)
+        print(json_response)
         if (json_response["success"] == True):
             message_id = json_response["messageId"]
     
     return message_id
 
-def get_message(message_id):
+def get_message(message_id, thenextleg_token):
     url = f"https://api.thenextleg.io/v2/message/{message_id}"
     headers = {
-        'Authorization': f'Bearer {os.environ["THE_NEXT_LEG_TOKEN"]}',
+        'Authorization': f'Bearer {thenextleg_token}',
     }
 
     json_response = None
@@ -45,45 +46,54 @@ def get_message(message_id):
         response = requests.request("GET", url, headers=headers)
         if (response.status_code == 200):
             json_response = json.loads(response.text)
+            print(json_response)
             break
     return json_response
 
-def get_images(message_id):
+def get_images(message_id, thenextleg_token):
     images = list()
+    response_content = None
     while True:
-        message = get_message(message_id)
+        message = get_message(message_id, thenextleg_token)
         if (message["progress"] == 100):
             progress = message["progress"]
             progress_bar.progress(progress, text=get_progress_text(progress))
+
             images.extend(message["response"]["imageUrls"])
+            response_content = message["response"]["content"]
             break
         else:
             progress = message["progress"]
             progress_bar.progress(progress, text=get_progress_text(progress))
-    return images
+    return (images, response_content)
 
 with st.form("prompt_form"):
-    st.write("AI Powered Image Generation")
-    prompt_input = st.text_area("")
+    st.write("TheNextLeg Driven Image Generation")
+    thenextleg_token = st.text_input(label="API Token", value="", type="password")
+    prompt_input = st.text_area(label="Prompt", value="")
     submitted = st.form_submit_button("Go")
     if submitted:
-        progress_bar = st.progress(0, text=get_progress_text(0))
-        message_id = imagine(prompt_input)
-        sleep(2)
-        images = get_images(message_id)
-        rows = []
-        row = []
-        for image in images:
-            row.append(image)
-            if len(row) == 2:
-                rows.append(row)
-                row = []
-        for row in rows:
-            col1, col2 = st.columns(2)
-            if (row[0]):
-                with col1:
-                    st.image(row[0])
+        with st.container():
+            progress_bar = st.progress(0, text=get_progress_text(0))
+            message_id = imagine(prompt_input, thenextleg_token)
+            sleep(2)
+            (images, response_content) = get_images(message_id, thenextleg_token)
+            st.text(response_content)
 
-            if (row[1]):
-                with col2:
-                    st.image(row[1])
+        with st.container():
+            rows = []
+            row = []
+            for image in images:
+                row.append(image)
+                if len(row) == 2:
+                    rows.append(row)
+                    row = []
+            for row in rows:
+                col1, col2 = st.columns(2)
+                if (row[0]):
+                    with col1:
+                        st.image(row[0])
+
+                if (row[1]):
+                    with col2:
+                        st.image(row[1])
